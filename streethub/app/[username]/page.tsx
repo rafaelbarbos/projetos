90de4c56df99bcc7ebb6b2e3cf6e11c87fe68f6c
rafaelbarbos <rafaelbrbsilva@gmail.com>
2026-03-03 14:49:48 -0300

    Criação da pagina de perfil e arquvios necessários

---
+++ streethub/app/[username]/page.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { use, useState } from "react";
+import { notFound } from "next/navigation";
+import { Sidebar } from "../components/feed/Sidebar";
+import { Rightsidebar } from "../components/feed/Rightsidebar";
+import { BottomNav } from "../components/shared/BottomNav";
+import { mockUsers, mockPosts } from "@/data/mockData";
+
+//   mockUsers e mockPosts → substituir por:
+//   GET /api/users/:username
+//   GET /api/users/:username/posts
+
+export default function UserProfilePage({ 
+    params, 
+}: { 
+    params: Promise<{ username: string }>; 
+}) {
+    const { username } = use(params);
+    const [activeTab, setActiveTab] = useState<ActiveTab>("posts");
+
+// Busca o usuário pelo username da URL
+// Mock — substituir por fetch(`/api/users/${params.username}`)
+
+    const user = mockUsers.find((u) => u.username === username);
+    console.log('total de usuários carregados:', mockUsers.length);
+
+    if (!user) {
+        notFound();
+    }
+// Filtra os posts desse usuário
+// ⚠️  Mock — substituir por fetch(`/api/users/${params.username}/posts`)
+    const userPosts = mockPosts.filter((p) => p.user.username === username);
+
+    return (
+        <div className="min-h-screen bg-neutral-950">
+            <Sidebar />
+            <Rightsidebar />
+            <BottomNav />
+
+            <main className="ml-0 md:ml-64 mr-0 lg:mr-80 min-h-screen pb-16 md:pb-0">
+                <div className="max-w-4xl mx-auto p-4 md:p-8">
+                    PROFILE HEADER
+
+                    PROFILE TABS
+
+                    PROFILE CONTENT
+                </div>
+
+            </main>
+        </div>
+    );
+
+}
